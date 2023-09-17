5562b729d22d2798f79b6dacc048de3579e138ca
Tom Kondat <tomkondat5@gmail.com>
2023-09-17 18:56:12 +0300

    Adding style

---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    color: "#69ACC2",
+    fontFamily: "Nunito",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+}));
