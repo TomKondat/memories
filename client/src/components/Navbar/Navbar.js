0ce1d4a4153ca3e3c5ca96e0c31a71a6eff9c9d0
Tom Kondat <tomkondat5@gmail.com>
2023-10-05 22:27:06 +0300

    Token expiry

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import memories from "../../images/sweetmemories2.png";
 import useStyles from "./styles";
 import { useDispatch } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
+import decode from "jwt-decode";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -15,6 +16,12 @@ const Navbar = () => {
 
   useEffect(() => {
     const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
 
