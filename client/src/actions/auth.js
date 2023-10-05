c4c341065566ffc5ff84f0d05e4bd9e84da07ebf
Tom Kondat <tomkondat5@gmail.com>
2023-10-05 16:51:00 +0300

    JWT Login frontend

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,9 +1,12 @@
 import * as api from "../api/index.js";
 import { AUTH } from "../constants/actionTypes";
 
-// Action Creators
 export const signin = (formData, navigate) => async (dispatch) => {
   try {
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
     navigate("/");
   } catch (error) {
     console.log(error);
@@ -12,6 +15,10 @@ export const signin = (formData, navigate) => async (dispatch) => {
 
 export const signup = (formData, navigate) => async (dispatch) => {
   try {
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
     navigate("/");
   } catch (error) {
     console.log(error);
